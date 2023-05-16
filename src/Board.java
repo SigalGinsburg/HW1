45a0205d7ec37e97d3450421e8f59fde84f9f91a
mayasokolov <mayasokolov@campus.technion.ac.il>
2023-05-16 18:31:22 +0300

    maya's first

--- src/Board.java
+++ src/Board.java
@@ -1,6 +1,29 @@
 import java.util.Arrays;
 
 public class Board {
+    private final int n;
+    private final int m;
+    private Tile [][] board;
+
+    public Board (String instruction){
+        String [] rows= instruction.split("\\|");
+        this.m= rows.length;
+        String [] row= rows[0].split(" ");
+        this.n=row.length;
+
+        board= new Tile[m][n];
+        for (int i=0; i<m; i++){
+            row=rows[i].split(" ");
+            for (int j=0; j<n; j++){
+                board[i][j]= new Tile(row[j]);
+            }
+        }
+
+    }
+
+    public Tile [][] getBoard (){
+        return this.board;
+    }
 
     @Override
     public boolean equals(Object other) {
